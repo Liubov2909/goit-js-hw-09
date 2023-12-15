9c3d94f717e7b76932b1e7dd3fdb1211c81f4724
Liubov2909 <ostapcuk2909@gmail.com>
2023-12-15 19:09:54 +0200

    dz9-js

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,4 +1,36 @@
 const form = document.querySelector(".feedback-form");
-const formEmail = form.querySelector("input");
-const textarea = form.querySelector("textarea");
+const formEmail = form.querySelector("input[name='email']");
+const formMessage = form.querySelector("textarea[name='message']");
 
+let obj = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+
+function setValue({ email, message }) {
+    formEmail.value = email || "";
+    formMessage.value = message || "";
+}
+
+setValue(obj);
+
+form.addEventListener('input', (event) => {
+    const element = event.target.name;
+    const value = event.target.value;
+    obj[element] = value;
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(obj));
+});
+
+form.addEventListener('submit', (event) => {
+    const emailValue = formEmail.value.trim();
+    const messageValue = formMessage.value.trim();
+
+    if (emailValue && messageValue) {
+        event.preventDefault();
+
+        localStorage.removeItem("feedback-form-state");
+        form.reset();
+
+        console.log(obj);
+    } else {
+        alert("Заповніть усі поля");
+    }
+});
